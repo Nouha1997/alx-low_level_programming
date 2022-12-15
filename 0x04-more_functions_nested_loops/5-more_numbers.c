2dc36551e95608c790a1ff8f45ded8b377d72b3e
Your Name <you@example.com>
2022-12-15 12:14:53 -0800

    morenumbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,23 +2,22 @@
 
 /**
  * more_numbers - Prints the numbers 0 to 14 ten times
+ *
  */
 
 void more_numbers(void)
 {
-	int n, count;
 
-	for (count = 0; count <= 9; count++)
+	int i, j;
+
+	for (i = 1; i <= 10; i++)
 	{
-		for (n = 0; n <= 14; n++)
+		for (j = 0; j <= 14; j++)
 		{
-			if (n < 9)
-			{
-				_putchar((n / 10) + '0');
-			}
-			_putchar((n % 10) + '0');
+			if (j >= 10)
+				_putchar('1');
+			_putchar(j % 10 + '0');
 		}
 		_putchar('\n');
 	}
-
 }
