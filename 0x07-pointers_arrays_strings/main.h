1c796e670b1f35abf99d4a53bc0dc38ba871672c
Your Name <you@example.com>
2023-01-02 14:21:39 -0800

    memset

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * the file is main.h 
+ */
+
+#include <stddef.h>
+
+int _putchar(char c);
+void *_memset(void *s, int c, size_t n);
+char *_memset(char *s, char b, unsigned int n);
+
+#endif
