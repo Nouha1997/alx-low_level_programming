2225768a0c4540f53a1998d8926dc2bd801b657a
Your Name <you@example.com>
2023-01-06 14:30:11 -0800

    myname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#inclue "main.h"
+
+/**
+ * main - Print the name of the program
+ *
+ * @arg: count arguments
+ *
+ * Return: always 0 (success)
+ */
+
+int main(int arg, char *argv[])
+
+{
+	(void)arc;
+
+	printf("%s\n", arg[0]);
+
+	return (0);
+
+}
