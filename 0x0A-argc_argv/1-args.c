6de984f41fdfea5bce532176de940d834a2922f2
Your Name <you@example.com>
2023-01-08 10:42:50 -0800

    args

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -9,10 +9,10 @@
  *
  * Return: Always 0 (Success)
  */
-int main(int argc, char *argv[]
+int main(int argc, char *argv[])
 {
 	(void) argv; /*Ignore argv*/
-	printf("%i\n", argc - 1);
+	printf("%d\n", argc - 1);
 	return (0);
 
 }
